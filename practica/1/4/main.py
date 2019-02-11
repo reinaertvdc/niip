00182d137b6cd669228fd8a3c7d8c0c441a06f46
Reinaert Van de Cruys <reinaert.vandecruys@gmail.com>
2019-02-11 21:44:27 +0100

    Add practicum 1, task 4.

---
+++ practica/1/4/main.py
@@ -0,0 +1,40 @@
+import Adafruit_GPIO.MCP230xx as MCP
+import Adafruit_GPIO.GPIO as GPIO
+
+import time
+import sys
+
+LED_PIN = 8
+BUTTON_PIN = 2
+ADDRESS = 0x20
+SLEEP_TIME = 0.1
+BUS_NUM = 1
+
+mcpi2c = MCP.MCP23017(ADDRESS, busnum=BUS_NUM)
+mcpi2c.setup(LED_PIN, MCP.GPIO.OUT)
+
+buttonState = False
+hasReleased = True
+
+while True:
+    try:
+        value = mcpi2c.input(BUTTON_PIN)
+
+        if value and hasReleased:
+            buttonState = not buttonState
+
+            if buttonState:
+                mcpi2c.output(LED_PIN, MCP.GPIO.HIGH)
+            else:
+                mcpi2c.output(LED_PIN, MCP.GPIO.LOW)
+
+            hasReleased = False
+
+        elif not value and not hasReleased:
+            hasReleased = True
+
+        time.sleep(SLEEP_TIME)
+
+    except KeyboardInterrupt:
+        mcpi2c.output(LED_PIN, MCP.GPIO.HIGH)
+        sys.exit()
