0313b13ce561d5e598d31d699f1bc0f2edd8f372
Brent Berghmans <brent.berghmans@gmail.com>
2019-04-14 15:31:39 +0200

    - Added DataProvider with small demo.

---
+++ project/node/node-master/dataprovider/dataprovider.ts
@@ -0,0 +1,235 @@
+import * as WebSocket from "ws";
+import * as uuid from "uuid/v4";
+
+interface DataSource {
+    key: string,
+    description: string,
+    source: Function,
+    thisObject: Object,
+    arguments: Array<any>
+}
+
+
+class DataProvider {
+    private static instance: DataProvider = null;
+    private dataSources: Map<string, DataSource> = new Map();
+    
+    private eventMap: Map<string, Function> = new Map();
+    private streamMap: Map<string, any> = new Map();
+
+    private server;
+    private wsServer;
+
+    private constructor() {
+        this.initEventMap();
+        this.createServer();
+    }
+
+    private initEventMap() {
+        this.onStartDataStream = this.onStartDataStream.bind(this);
+        this.onStopDataStream = this.onStopDataStream.bind(this);
+        this.onListData = this.onListData.bind(this);
+        this.onGetData = this.onGetData.bind(this);
+        this.getData = this.getData.bind(this);
+        this.getMultipleData = this.getMultipleData.bind(this);
+        this.eventMap.set("start-data-stream", this.onStartDataStream);
+        this.eventMap.set("stop-data-stream", this.onStopDataStream);
+        this.eventMap.set("list-data", this.onListData);
+        this.eventMap.set("get-data", this.onGetData);
+    }
+
+    private createServer() {
+        this.wsServer = new WebSocket.Server({
+            port: 8945
+        });
+
+        this.wsServer.on("connection", (connection) => {
+            this.onConnection(connection);
+        });
+    }
+
+    private onConnection(connection) {
+        connection.on("message", (message) => {
+            console.log("[DataProvider] Received message: " + message);
+            let parsed = JSON.parse(message);
+            this.onMessage(connection, parsed);
+        });
+
+        connection.on("error", (error) => {
+            console.log("Error=========");
+            console.log(error);
+        });
+    }
+
+    private onMessage(connection, message) {
+        if(message.hasOwnProperty("type") && message.hasOwnProperty("arguments")) {
+            var type = message.type;
+            
+            if(this.eventMap.has(type)) {
+                this.eventMap.get(type)(connection, message.arguments);
+            }
+        }
+    }
+
+    private onGetData(connection, payload) {
+        if(payload.hasOwnProperty("sources")) {
+            var sources = payload.sources;
+            var reply = {
+                "type": "get-data", 
+                "data": {
+                    
+                }
+            };
+
+            this.getMultipleData(sources).then((output) => {
+                reply.data = output;
+                connection.send(JSON.stringify(reply));
+            })
+            .catch((error) => {
+                console.log(error);
+            });
+        }
+        else {
+            this.sendError(connection, "Wrong format");
+        }
+    }
+
+    private onStartDataStream(connection, payload) {
+        if(payload.hasOwnProperty("sources") &&
+           payload.hasOwnProperty("interval")
+        ) {
+            var streamUUID = uuid();
+            var interval = setInterval(() => {
+                this.getMultipleData(payload.sources).then((data) => {
+                    var reply = {
+                        "type": "data-stream-tick",
+                        "data": data
+                    }
+                    reply.data["uuid"] = streamUUID;
+
+                    connection.send(JSON.stringify(reply))
+                })
+                .catch((error) => {
+                    console.log("[DataProvider] Error on interval " + streamUUID + ", closing connection.");
+                    clearInterval(interval);
+                    this.streamMap.delete(streamUUID);
+                    connection.terminate();
+                });
+            }, payload.interval);
+
+            this.streamMap.set(streamUUID, interval);
+
+            connection.send(JSON.stringify({
+                "type": "start-data-stream",
+                "data": {
+                    "uuid": streamUUID
+                }
+            }));
+        } 
+        else {
+            this.sendError(connection, "Wrong format.");
+        }
+    }
+
+    private onStopDataStream(connection, payload) {
+        if(payload.hasOwnProperty("uuid")) {
+            if(this.eventMap.has(payload.uuid)) {
+                clearInterval(payload.uuid);
+                connection.send(JSON.stringify({
+                    "type": "stop-data-stream",
+                    "data": {
+                        "success": true
+                    }
+                }));
+            }
+            else {
+                this.sendError(connection, "UUID not found.");
+            }
+        }
+        else {
+            this.sendError(connection, "Wrong format.");
+        }
+    }
+
+    private onListData(connection, payload) {
+        var sourcesIt = this.dataSources.values();
+        var sources = [];
+
+        let result = sourcesIt.next();
+        while(!result.done) {
+            sources.push(result.value);
+            result = sourcesIt.next();
+        }
+
+        connection.send(JSON.stringify({
+            "type": "list-data",
+            "data": {
+                "sources": sources
+            }
+        }));
+    }
+
+    private sendError(connection, error) {
+        connection.send(JSON.stringify({
+            "type": "error", 
+            "data": error
+        }));
+    }
+
+    public static getInstance(): DataProvider {
+        if(DataProvider.instance == null) {
+            DataProvider.instance = new DataProvider();
+        }
+        return DataProvider.instance;
+    }
+
+    public has(key: string) {
+        return this.dataSources.has(key);
+    }
+
+    public getSource(key: string) {
+        return this.dataSources.get(key);
+    }
+
+    public getData(key: string) {
+        var source = this.dataSources.get(key);
+        var callback = source.source;
+        var args = source.arguments;
+
+        return callback.apply(source.thisObject, args);
+    }
+
+    public getMultipleData(keys: Array<string>) {
+        return new Promise((resolve, reject) => {
+            let response = {};
+            let found = [];
+
+            for(var i = 0; i < keys.length; i++) {
+                if(this.has(keys[i])) {
+                    found.push(keys[i]);
+                }
+                else {
+                    response[keys[i]] = null;
+                }
+            }
+            
+            let keySet: Set<string> = new Set(found);
+            found.forEach((value, index) => {
+                this.getData(value).then((output) => {
+                    response[value] = output;
+                    keySet.delete(value);
+
+                    if(keySet.size == 0) {
+                        resolve(response);
+                    }
+                });
+            });
+        });
+    }
+
+    public register(source: DataSource) {
+        this.dataSources.set(source.key, source);
+    }
+}
+
+export { DataProvider };
--- project/node/node-master/index.ts
+++ project/node/node-master/index.ts
@@ -1,3 +1,4 @@
+/*
 import { BluetoothOBD2 } from "./obd2/bluetooth-obd2";
 
 let obd2 = new BluetoothOBD2("OBDII", "/dev/rfcomm0", {
@@ -12,6 +13,24 @@ obd2.init().then(() => {
 			console.log(value);
 		});
 	});
+});
+*/
+
+import { DataProvider } from "./dataprovider/dataprovider"
+
+function randInt(min, max) {
+	return new Promise((resolve, reject) => {
+		resolve(Math.floor(Math.random() * (max - min)) + min);
+	});
+}
+
+var provider: DataProvider = DataProvider.getInstance();
+provider.register({
+	"key": "rand-int-10-50",
+	"description": "Gets a random integer between 10 and 50.",
+	"source": randInt,
+	"thisObject": null,
+	"arguments": [10, 50]
 });
 
 // This is to stop node from closing
