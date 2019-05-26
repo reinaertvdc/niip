8445a7cc2c3a173082a4855dcc2947353da9c869
Wout Coenen <cwout.coenen@gmail.com>
2019-05-26 10:37:18 +0200

    react: node location is shown in node dashboard

--- project/root/react/logitrack/src/pages/Dashboard/DashboardCompany/DashboardNode/index.tsx
+++ project/root/react/logitrack/src/pages/Dashboard/DashboardCompany/DashboardNode/index.tsx
@@ -2,6 +2,7 @@ import * as React from 'react';
 import { Redirect } from 'react-router';
 import { DASHBOARD } from '../../../../constants/routes';
 import NumberGraph, {NumberGraphDataSet} from '../../../../components/NumberGraph';
+import NodeGoogleMap, { NodeLocation } from '../../../../components/NodeGoogleMap';
 
 export interface IDashboardNodeProps {
   match: {
@@ -13,7 +14,8 @@ export interface IDashboardNodeProps {
 }
 
 export interface IDashboardNodeState {
-  tmpData: NumberGraphDataSet
+  tmpData: NumberGraphDataSet,
+  location: NodeLocation|undefined
 }
 
 const initialNumberGraphDataSet: NumberGraphDataSet = {label:'',points:[]}
@@ -23,25 +25,51 @@ export default class DashboardNode extends React.Component<IDashboardNodeProps, 
     super(props);
 
     this.state = {
-      tmpData: initialNumberGraphDataSet
+      tmpData: initialNumberGraphDataSet,
+      location: undefined
     }
   }
 
   private async getNodeData(): Promise<void> {
+    //TODO: remove timeout
     //TODO: get node data from API
-    this.setState({
-      tmpData: {
-        label:'speed',
-        points:[{x:1,y:1},{x:5,y:2},{x:6,y:3},{x:7,y:2}]
-      }
-    })
+    setTimeout(() => {
+      this.setState({
+        tmpData: {
+          label:'speed',
+          points:[{x:1,y:1},{x:5,y:2},{x:6,y:3},{x:7,y:2}]
+        }
+      })
+    }, 1000);
+  }
+
+  private async getNodeLocation(): Promise<void> {
+    //TODO: remove timeout
+    //TODO: get node data from API
+    setTimeout(() => {
+      this.setState({
+        location: {
+          lbl:'truck ' + this.props.match.params.id,
+          id: parseInt(this.props.match.params.id),
+          lat:50.9312154, lng:5.3935026
+        }
+      });
+    }, 2000);
   }
 
   public componentDidMount() {
     //TODO: remove timeout
-    setTimeout(() => {
-      this.getNodeData();
-    }, 1000);
+    this.getNodeData();
+    this.getNodeLocation();
+  }
+
+  private Location = () => {
+    if (this.state.location !== undefined) {
+      return (
+        <NodeGoogleMap locations={[this.state.location]} />
+      )
+    }
+    return (null);
   }
 
   public render() {
@@ -56,6 +84,7 @@ export default class DashboardNode extends React.Component<IDashboardNodeProps, 
     return (
       <div>
         <NumberGraph data={this.state.tmpData} />
+        <this.Location />
       </div>
     );
   }
